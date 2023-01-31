7901a985bae9f9c3f1a4d265c830b09d6faa10e8
JoaoPaulo-creator <joaosarto@gmail.com>
2023-01-30 21:14:11 -0300

    MVP finalizado

--- src/pages/product-detail/index.tsx
+++ src/pages/product-detail/index.tsx
@@ -1,25 +1,182 @@
-export default function ProductDetail(){
-    return (
 
-        <div className="max-w-3xl mx-auto">
-            <div className="w-full my-4">
-                <img src="whey.webp" alt="product-image" className="w-full h-64 object-cover max-h-fit"/>
-                <div className="p-4">
-                <h2 className="text-lg font-medium">WHEY PROTEIN</h2>
-                <p className="text-gray-600">asdasd</p>
-                <p className="text-red-600 font-medium">$123123123</p>
+import { StarIcon } from '@heroicons/react/20/solid'
+
+
+const product = {
+
+
+    name: 'Basic Tee 6-Pack',
+    price: '$192',
+    href: '#',
+    breadcrumbs: [
+        { id: 1, name: 'Geral', href: '/' },
+        { id: 2, name: 'Protein', href: '/' },
+    ],
+    colors: [
+        { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+        { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+        { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    ],
+    
+    highlights: [
+        'Whey protein top',
+        'Perfeito para sua dieta',
+        'Show mesmo',
+    ],
+    details:
+        'O Whey 3w contem muita proteina e mineirais perfeito para manter sua saude em dia',
+    }
+    const reviews = { href: '#', average: 4, totalCount: 117 }
+    
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+
+export default function ProductDetail(){
+
+    return (
+        <div className="bg-white">
+            <div className="pt-6">
+                <nav aria-label="Breadcrumb">
+                <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                    {product.breadcrumbs.map((breadcrumb) => (
+                    <li key={breadcrumb.id}>
+                        <div className="flex items-center">
+                        <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
+                            {breadcrumb.name}
+                        </a>
+                        <svg
+                            width={16}
+                            height={20}
+                            viewBox="0 0 16 20"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                            aria-hidden="true"
+                            className="h-5 w-4 text-gray-300"
+                        >
+                            <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                        </svg>
+                        </div>
+                    </li>
+                    ))}
+                    <li className="text-sm">
+                    <a href={product.href} aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
+                        {product.name}
+                    </a>
+                    </li>
+                </ol>
+                </nav>
+
+                {/* Image gallery */}
+                <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+                    <div className="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
+                        <img
+                        src='whey.webp'
+                        alt='product image'
+                        className="h-full w-full object-cover object-center"
+                        />
+                </div>
+                <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+                    <div className="aspect-w-3 aspect-h-2 overflow-hidden rounded-lg">
+                        <img
+                            src='whey.webp'
+                            alt='product image'
+                            className="h-full w-full object-cover object-center"
+                        />
+                    </div>
+                    <div className="aspect-w-3 aspect-h-2 overflow-hidden rounded-lg">
+                        <img
+                            src='whey.webp'
+                            alt='product image'
+                            className="h-full w-full object-cover object-center"
+                        />
+                    </div>
+                </div>
+
+                <div className="mt-4 lg:row-span-3 lg:mt-0">
+                    <h2 className="sr-only">Product information</h2>
+                    <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
+
+                    {/* Reviews */}
+                    <div className="mt-6">
+                        <h3 className="sr-only">Reviews</h3>
+                        <div className="flex items-center">
+                            <div className="flex items-center">
+                            {[0, 1, 2, 3, 4].map((rating) => (
+                                <StarIcon
+                                key={rating}
+                                className={classNames(
+                                    reviews.average > rating ? 'text-gray-900' : 'text-gray-200',
+                                    'h-5 w-5 flex-shrink-0'
+                                )}
+                                aria-hidden="true"
+                                />
+                            ))}
+                            </div>
+                            <p className="sr-only">{reviews.average} out of 5 stars</p>
+                            <a href={reviews.href} className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                            {reviews.totalCount} reviews
+                            </a>
+                        </div>
+                    </div>
+
+                        <form className="mt-10">
+                            <button
+                                type="submit"
+                                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                Add to bag
+                            </button>
+                        </form>
+                    </div>
+                
+                </div>
+
+
+                {/* Product info */}
+                <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
+                <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                    <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
+                </div>
+
+            
+                
+
+                <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
+                    {/* Description and details */}
+                    <div>
+                    <h3 className="sr-only">Description</h3>
+
+                    <div className="space-y-6">
+                        <p className="text-base text-gray-900">Whey Protein hidrolisado, isolado e concentrado</p>
+                    </div>
+                    </div>
+
+                    <div className="mt-10">
+                    <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+
+                    <div className="mt-4">
+                        <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                        {product.highlights.map((highlight) => (
+                            <li key={highlight} className="text-gray-400">
+                            <span className="text-gray-600">{highlight}</span>
+                            </li>
+                        ))}
+                        </ul>
+                    </div>
+                    </div>
+
+                    <div className="mt-10">
+                    <h2 className="text-sm font-medium text-gray-900">Details</h2>
+
+                    <div className="mt-4 space-y-6">
+                        <p className="text-sm text-gray-600">{product.details}</p>
+                    </div>
+                    </div>
+                </div>
                 </div>
             </div>
-            <div className="my-4">
-                <h2 className="text-lg font-medium">Product Details</h2>
-                <p className="text-gray-600">descricao do produto</p>
-            </div>
-        <div className="my-4">
-            <h2 className="text-lg font-medium">Product Features</h2>
-            <ul className="text-gray-600">
-            
-            </ul>
         </div>
-    </div>
     )
 }
